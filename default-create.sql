dc985de68709773c2dbcec4890412b22d03b626b
Mikko Oksa <m3tacity@gmail.com>
2014-04-05 01:25:25 +0300

    Initial commit (sql gen scripts)

---
+++ default-create.sql
@@ -0,0 +1,30 @@
+create table city (
+  id                        bigint auto_increment not null,
+  name                      varchar(255) not null,
+  constraint pk_city primary key (id))
+;
+
+create table dogpark (
+  id                        bigint auto_increment not null,
+  name                      varchar(255) not null,
+  latitude                  double not null,
+  longitude                 double not null,
+  city_id                   bigint,
+  constraint pk_dogpark primary key (id))
+;
+
+create table dogpark_signup (
+  id                        bigint auto_increment not null,
+  arrival_time              datetime not null,
+  dog_breed                 varchar(255),
+  dog_weight                integer not null,
+  dogpark_id                bigint,
+  constraint pk_dogpark_signup primary key (id))
+;
+
+alter table dogpark add constraint fk_dogpark_city_1 foreign key (city_id) references city (id) on delete restrict on update restrict;
+create index ix_dogpark_city_1 on dogpark (city_id);
+alter table dogpark_signup add constraint fk_dogpark_signup_dogPark_2 foreign key (dogpark_id) references dogpark (id) on delete restrict on update restrict;
+create index ix_dogpark_signup_dogPark_2 on dogpark_signup (dogpark_id);
+
+
---
+++ default-drop.sql
@@ -0,0 +1,10 @@
+SET FOREIGN_KEY_CHECKS=0;
+
+drop table city;
+
+drop table dogpark;
+
+drop table dogpark_signup;
+
+SET FOREIGN_KEY_CHECKS=1;
+
