f378ced5f4ee4fce0d79d29cf129853b141b886b
Mikko Oksa <m3tacity@gmail.com>
2014-04-05 01:24:48 +0300

    Add controller method for dogpark listing and details

--- src/main/java/controllers/DogParkController.java
+++ src/main/java/controllers/DogParkController.java
@@ -21,18 +21,66 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-
 package controllers;
 
+import com.avaje.ebean.EbeanServer;
+import com.avaje.ebean.annotation.Transactional;
+import com.google.common.collect.Lists;
+import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import java.util.List;
+import java.util.logging.Logger;
+import models.City;
+import models.DogPark;
 import ninja.Result;
 import ninja.Results;
+import ninja.params.Param;
+
 
 @Singleton
 public class DogParkController {
 
+	private final Logger logger;
+	private final EbeanServer ebeanServer;
+
+	@Inject
+	public DogParkController(Logger logger, EbeanServer ebeanServer) {
+		this.logger = logger;
+		this.ebeanServer = ebeanServer;
+	}
+
 	public Result index() {
 		return Results.html();
+	}
+
+	public Result dogparkList() {
+		List<City> cities = ebeanServer.find(City.class).fetch(City.FETCH_DOGPARKS).findList();
+		return Results.html().render("cities", cities);
+	}
+
+	public Result dogpark(@Param("id") Long dogParkId) {
+		if (dogParkId == null) {
+			return Results.html().template("views/system/404notFound.ftl.html");
+		}
+
+		DogPark dogPark = ebeanServer.find(DogPark.class, dogParkId);
+		if (dogPark != null) {
+			return Results.html().render("dogPark", dogPark);
+		} else {
+			return Results.html().template("views/system/404notFound.ftl.html");
+		}
+	}
+
+	@Transactional
+	public Result setupTables() {
+		City kuopio = new City("Kuopio");
+		List<DogPark> kuopioDogParks = Lists.newArrayList(
+				new DogPark("Neulamäen koirapuisto", 62.887032, 27.609753, kuopio),
+				new DogPark("Rypysuon koirapuisto", 62.918761, 27.636628, kuopio)
+		);
+		ebeanServer.save(kuopio);
+		ebeanServer.save(kuopioDogParks);
+		return Results.text().renderRaw("Tables ok");
 	}
 
 }
