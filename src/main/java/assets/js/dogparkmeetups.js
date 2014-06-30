43cf881a205a3fdccf3245967aefdb006941a7a8
Mikko Oksa <m3tacity@gmail.com>
2014-06-30 22:31:42 +0300

    Throw some code away, validate signup form

--- src/main/java/assets/js/dogparkmeetups.js
+++ src/main/java/assets/js/dogparkmeetups.js
@@ -6,168 +6,144 @@ $(document).ajaxStart(function() {
 
 // -------------------------------------------------------------------
 
-var navTab;
-var dogparkListContainer;
 var dogparkContainer;
 
-var calendarOptions;
-
-var visiblePopoversAnchor;
-
 $(document).ready(function() {
-	navTab = $('#nav-tab');
-	dogparkListContainer = $('#dogpark-list-container');
-	dogparkContainer = $('#dogpark-container');
-		
-	$('body').click(function(e) {
-		 dogparkContainer.find('.day a').each(function() {
-			if ($('.popover .close').is(e.target) || (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0)) {
-				$(this).popover('destroy');
-			}
-		});
-	});
-
-	navTab.find('a').click(function(e) {
-		e.preventDefault();
-		$(this).tab('show');
-	});
 	
-	dogparkContainer.on('submit', '.signup-form', function(e) {
-		e.preventDefault();
-		var dogparkId = $(this).data('dogparkId');
-		$.post('dogparks/' + dogparkId + '/signups', $(this).serialize(), function(signup) {
-			var day = calendarOptions.events[signup.date];
-			if (!day) {
-				day = {
-					number: 0,
-					badgeClass: 'badge-warning',
-					dayEvents: []
-				};
-			}
-			day.dayEvents.push(signup);
-			day.number = day.dayEvents.length;
-			calendarOptions.events[signup.date] = day;
-			dogparkContainer.find('.responsive-calendar').responsiveCalendar('edit', calendarOptions.events);
-			visiblePopoversAnchor.popover('destroy');
-			visiblePopoversAnchor.trigger('click');
-		}, 'json');
-	});
-
-	navTab.find('a[href=#dogpark-list]').on('shown.bs.tab', loadDogParkList);
-	dogparkListContainer.on('click', 'a', loadDogPark);
+	dogparkContainer = $('#dogpark');
+	if (dogparkContainer.length) {
+		$('body').click(function(e) {
+			dogparkContainer.find('.day a').each(function() {
+				if ($('.popover .close').is(e.target) 
+					|| 
+					 (!$(this).is(e.target)
+						&& $(this).has(e.target).length === 0 
+						&& $('.popover').has(e.target).length === 0)
+					) {
+					
+					$(this).popover('destroy');
+				}
+			});
+		});
+		loadCalendar();
+	}
+	
+	$('#signup-form').submit(validateSignupForm);
 });
 
-function loadDogParkList() {
-	dogparkListContainer.html("");
-	$.get('dogparks', function(dogparkList) {
-		dogparkListContainer.html(dogparkList);
-	});
-}
-
-function loadDogPark() {
-	var dogparkId = $(this).data('dogparkId');
-	var dogparkName = $(this).text();
-
-	navTab.find('a[href=#dogpark]').tab('show');
-	$('#dogpark > h3').html('<i class="glyphicon glyphicon-tree-deciduous"></i> ' + dogparkName);
-
-	dogparkContainer.html("");
-	calendarOptions = {
+function loadCalendar() {
+	var calendarOptions = {
 		translateMonths: [
 			'Tammikuu', 'Helmikuu', 'Maaliskuu', 'Huhtikuu', 'Toukokuu', 'Kesäkuu', 
 			'Heinäkuu', 'Elokuu', 'Syyskuu', 'Lokakuu', 'Marraskuu', 'Joulukuu'
 		],
-		onDayClick: funcForPopoverShowing(dogparkId)
+		onDayClick: showSignupsPopup
 	};
-	var dogparkHtml;
+
+	var now = new Date();
+	$.getJSON(window.location.href + '/signups?yearMonth=' + now.getFullYear() + '-' + pad(now.getMonth()+1), function(signups) {
+		var calendarEvents = groupByDate(signups);
+		calendarOptions.events = calendarEvents;
+		console.log(calendarOptions);
+		$('#dogpark-calendar').responsiveCalendar(calendarOptions);
+	});
+}
+
+// Forms the 'events' object like described at http://w3widgets.com/responsive-calendar/#options
+function groupByDate(signups) {
+	var optionsByDate = {};
 	
-	$.when(
-			$.get('dogparks/' + dogparkId, function(dogpark) {
-				dogparkHtml = dogpark;
-			}),
-			$.getJSON('dogparks/' + dogparkId + '/signups?year=' + new Date().getFullYear() + '&month=' + (new Date().getMonth()+1), function(signupEvents) {
-				calendarOptions.events = signupEvents;
-			})
-		).then(function() {
-			dogparkContainer.html(dogparkHtml);
-			dogparkContainer.find('.responsive-calendar').responsiveCalendar(calendarOptions);
-		});
-}
+	$.each(signups, function(i, signup) {
+		var arrivalTime = new Date(signup.arrivalTime);
+		var dateString = toLocalDateString(arrivalTime);
 
-function addLeadingZero(num) {
-	if (num < 10) {
-		return "0" + num;
-	} else {
-		return "" + num;
-	}
-}
-
-function funcForPopoverShowing(dogparkId) {
-	return function(events) {
-		var thisDayEvents,
-			key,
-			eventsHtml;
-
-		key = $(this).data('year')+ '-' 
-				+ addLeadingZero($(this).data('month')) + '-' 
-				+ addLeadingZero($(this).data('day'));
-		thisDayEvents = events[key] ? events[key].dayEvents : [];
-
-		eventsHtml = '<ul class="list-unstyled">';
-		if (thisDayEvents.length > 0) {
-			for (var i = 0; i < thisDayEvents.length; ++i) {
-				eventsHtml += '<li><b>' + thisDayEvents[i].hour + ':</b> ' + thisDayEvents[i].name + '</li>';
-			}
-		} else {
-			eventsHtml += '<li>Ei lmoittautuneita</li>';
+		// Init the options for the date if necessary
+		if (optionsByDate.hasOwnProperty(dateString) === false) {
+			optionsByDate[dateString] = {
+				badgeClass: 'badge-warning',
+				dayEvents: []
+			};
 		}
-		eventsHtml += '</ul><hr> \
-				<form role="form" class="signup-form" data-dogpark-id="' + dogparkId + '">\
-					<input name="date" value="' + key + '" type="hidden">\
-					<div class="form-group"> \
-						<label for="timeOfArrival">Saapumisaika <small>(esim. 13:30 tai 14.45)</small></label> \
-							<input type="text" class="form-control" name="timeOfArrival" id="timeOfArrival" placeholder="Syötä kellonaika"> \
-					</div> \
-					<div class="form-group"> \
-						<label for="dogbreed">Koiran rotu</label> \
-							<input type="text" class="form-control" name="dogBreed" id="dogbreed" placeholder="Syötä rotu"> \
-					</div> \
-					<div class="form-group"> \
-						<label for="dogweightclass">Koiran paino</label> \
-						<select class="form-control" name="dogWeightClass" id="dogweightclass"> \
-							<option value="KG_1_TO_5">1-5 kg</option> \
-							<option value="KG_5_TO_10">5-10 kg</option> \
-							<option value="KG_10_TO_15">10-15 kg</option> \
-							<option value="KG_15_TO_25">15-25 kg</option> \
-							<option value="KG_25_TO_40">25-40 kg</option> \
-							<option value="KG_40_PLUS">40+ kg</option> \
-						</select> \
-					</div> \
-					<div class="form-group"> \
-						<label class="radio-inline"> \
-							<input type="radio" name="dogIsMale" id="dogIsMale1" value="true" checked> \
-							Uros \
-						</label> \
-						<label class="radio-inline"> \
-							<input type="radio" name="dogIsMale" id="dogIsMale1" value="false"> \
-							Narttu \
-						</label> \
-					</div> \
-					<div class="centered"><button type="submit" class="btn btn-primary">Ilmoittaudu!</button></div> \
-				</form>';
+		
+		optionsByDate[dateString].dayEvents.push({
+			name: signup.dogBreed + ' (' + signup.dogWeightClass + ' ' + (signup.dogIsMale ? 'uros' : 'naaras') + ')',
+			hour: pad(arrivalTime.getHours()) + ':' + pad(arrivalTime.getMinutes())
+		});		
+	});
+	
+	$.each(optionsByDate, function(date, options) {
+		options.number = options.dayEvents.length;
+	});
+	
+	return optionsByDate;
+}
 
-		visiblePopoversAnchor = $(this);
-		visiblePopoversAnchor.popover({
-			trigger: 'manual',
-			html: true,
-			animation: false,
-			placement: 'auto top',
-			title: 'Ilmoittautuneet (' + key + ') <button type="button" class="pull-right close" aria-hidden="true">&times;</button>',
-			content: eventsHtml,
-			container: dogparkContainer
-		});
-		dogparkContainer.find('.day a').not(this).popover('destroy');
-		$(this).popover('toggle');
-		return false;
-	};
+function toLocalDateString(date) {
+	return date.getFullYear() + '-' + pad(date.getMonth()+1) + '-' + pad(date.getDate());
+}
+
+function pad(num) {
+	return (num < 10 ? '0' : '') + num;
+}
+
+function showSignupsPopup(events) {
+	var thisDayEvents,
+	    key,
+	    eventsHtml;
+
+	key = $(this).data('year') + '-'
+			+ pad($(this).data('month')) + '-'
+			+ pad($(this).data('day'));
+	thisDayEvents = events[key] ? events[key].dayEvents : [];
+
+	eventsHtml = '<ul class="list-unstyled">';
+	if (thisDayEvents.length > 0) {
+		for (var i = 0; i < thisDayEvents.length; ++i) {
+			eventsHtml += '<li><b>' + thisDayEvents[i].hour + ':</b> ' + thisDayEvents[i].name + '</li>';
+		}
+	} else {
+		eventsHtml += '<li>Ei lmoittautuneita</li>';
+	}
+	eventsHtml += '</ul><hr> \
+		<div class="centered"> \
+			<a href="' + window.location.href + '/signup?date=' + key + '" class="btn btn-primary">Ilmoittaudu</a> \
+		</div>';
+
+	$(this).popover({
+		trigger: 'manual',
+		html: true,
+		animation: false,
+		placement: 'auto top',
+		title: 'Ilmoittautuneet (' + key + ') <button type="button" class="pull-right close" aria-hidden="true">&times;</button>',
+		content: eventsHtml,
+		container: dogparkContainer
+	});
+	//dogparkContainer.find('.day a').not(this).popover('destroy');
+	$(this).popover('toggle');
+	//return false;
+}
+
+// Some refactoring needed..
+function validateSignupForm(event) {
+	var hasErrors = false;
+	var timeInput  = $(this).find('#timeOfArrival:first').val().trim().replace('\.', ':');
+	
+	var re = new RegExp('^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$');
+	if (re.test(timeInput) === false) {
+		$(this).find('.form-group:first').removeClass('has-success').addClass('has-error');
+		hasErrors = true;
+	} else {
+		$(this).find('.form-group:first').removeClass('has-error').addClass('has-success');
+	}
+	
+	if ($(this).find('#dogbreed:first').val().trim().length === 0) {
+		$(this).find('.form-group:eq(1)').removeClass('has-success').addClass('has-error');
+		hasErrors = true;
+	} else {
+		$(this).find('.form-group:eq(1)').removeClass('has-error').addClass('has-success');
+	}
+	
+	if (hasErrors) {
+		event.preventDefault();
+	}
 }
