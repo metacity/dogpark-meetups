84e33619635681a84edfa58d5cfa7f9564a38e30
metacity (Mikko Oksa) <mikkoks@uef.fi>
2014-07-03 15:32:29 +0300

    Implement signup cancellation

--- src/main/java/controllers/DogparkController.java
+++ src/main/java/controllers/DogparkController.java
@@ -28,10 +28,13 @@ import com.basho.riak.client.RiakException;
 import com.basho.riak.client.RiakFactory;
 import com.basho.riak.client.RiakRetryFailedException;
 import com.basho.riak.client.bucket.Bucket;
+import com.google.common.base.Strings;
 import com.google.common.escape.Escaper;
 import com.google.common.html.HtmlEscapers;
 import com.google.inject.Inject;
+import com.google.inject.Provider;
 import com.google.inject.Singleton;
+import java.nio.charset.StandardCharsets;
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -40,6 +43,7 @@ import java.time.ZoneId;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Base64;
 import java.util.Collections;
 import java.util.Date;
 import java.util.List;
@@ -51,11 +55,13 @@ import models.DogparkSignup;
 import models.DogparkSignupList;
 import ninja.Result;
 import ninja.Results;
-import ninja.Router;
 import ninja.lifecycle.Dispose;
 import ninja.lifecycle.Start;
 import ninja.params.Param;
 import ninja.params.PathParam;
+import ninja.postoffice.Mail;
+import ninja.postoffice.Postoffice;
+import ninja.session.FlashScope;
 import ninja.utils.NinjaProperties;
 import org.slf4j.Logger;
 
@@ -70,18 +76,26 @@ public class DogparkController {
 
 	public static final String VIEW_404_NOT_FOUND = "views/system/404notFound.ftl.html";
 
+	// Guice injected fields
 	private final Logger logger;
 	private final NinjaProperties properties;
-	private final Router router;
+	private final Provider<Mail> mailProvider;
+	private final Postoffice postoffice;
 
 	private IRiakClient riakClient;
 	private Bucket bucket;
 
 	@Inject
-	public DogparkController(Logger logger, NinjaProperties properties, Router router) {
+	public DogparkController(
+			Logger logger, 
+			NinjaProperties properties, 
+			Provider<Mail> mailProvider, 
+			Postoffice postoffice) {
+		
 		this.logger = logger;
 		this.properties = properties;
-		this.router = router;
+		this.mailProvider = mailProvider;
+		this.postoffice = postoffice;
 	}
 
 	@Start(order = 10)
@@ -101,22 +115,26 @@ public class DogparkController {
 			riakClient.shutdown();
 		}
 	}
-
-	public Result index() {
-		return Results.html();
+	
+	public Result allDogparks() throws RiakRetryFailedException {
+		CityList cities = bucket.fetch(KEY_CITIES, CityList.class).execute();
+		List<Dogpark> dogparks = cities.stream()
+				.flatMap(city -> city.dogparks.stream())
+				.collect(Collectors.toList());
+		return Results.json().render(dogparks);
 	}
 
-	public Result dogparkList() throws RiakRetryFailedException {
+	public Result dogparkListPage() throws RiakRetryFailedException {
 		CityList cities = bucket.fetch(KEY_CITIES, CityList.class).execute();
 		return Results.html().render("cities", cities);
 	}
 
-	public Result dogpark(@PathParam("id") String dogparkId) throws RiakRetryFailedException {
+	public Result dogparkPage(@PathParam("id") String dogparkId) throws RiakRetryFailedException {
 		if (dogparkId == null) {
 			return Results.notFound().html().template(VIEW_404_NOT_FOUND);
 		}
 
-		Optional<Dogpark> dogpark = getDogpark(dogparkId);
+		Optional<Dogpark> dogpark = getDogparkFromDb(dogparkId);
 		if (dogpark.isPresent()) {
 			return Results.html().render("dogpark", dogpark.get());
 		} else {
@@ -160,14 +178,14 @@ public class DogparkController {
 		
 	}
 
-	public Result signup(
+	public Result signupPage(
 			@PathParam("id") String dogparkId,
 			@Param("date") String date) throws RiakRetryFailedException {
 
 		// Parse to validate
-		LocalDate localDate = LocalDate.parse(date);
+		LocalDate localDate = LocalDate.parse(date.trim());
 
-		Optional<Dogpark> dogpark = getDogpark(dogparkId);
+		Optional<Dogpark> dogpark = getDogparkFromDb(dogparkId);
 		if (dogpark.isPresent()) {
 			return Results.html().render("dogpark", dogpark.get()).render("date", date);
 		}
@@ -175,10 +193,17 @@ public class DogparkController {
 	}
 
 	public Result doSignupPost(
+			FlashScope flashScope,
 			@PathParam("id") String dogparkId,
 			@Param("date") String date,
 			@Param("timeOfArrival") String timeOfArrival,
+			@Param("email") String email,
 			DogparkSignup newSignup) throws RiakRetryFailedException {
+		
+		Optional<Dogpark> dogpark = getDogparkFromDb(dogparkId);
+		if (!dogpark.isPresent()) {
+			return Results.notFound().html().template(VIEW_404_NOT_FOUND);
+		}
 		
 		// Escape against HTML 
 		Escaper htmlEscaper = HtmlEscapers.htmlEscaper();
@@ -189,33 +214,113 @@ public class DogparkController {
 		// Generate UUID cancellation code
 		newSignup.generateCancellationCode();
 
-		timeOfArrival = timeOfArrival.replace('.', ':');
+		// Parse and set arrival time
+		timeOfArrival = timeOfArrival.trim().replace('.', ':');
 		LocalDateTime arrivalTimestamp = LocalDateTime.parse(date + " " + timeOfArrival,
 				DateTimeFormatter.ofPattern("yyyy-MM-dd H:m"));
-
 		ZoneId zoneId = ZoneId.of("Europe/Helsinki");
 		newSignup.arrivalTime = Date.from(arrivalTimestamp.toInstant(ZonedDateTime.now(zoneId).getOffset()));
 
+		// Add to signups list
 		DogparkSignupList signups = bucket.fetch(dogparkId, DogparkSignupList.class).execute();
 		if (signups == null) {
 			signups = new DogparkSignupList();
 		}
 		signups.add(newSignup);
 		bucket.store(dogparkId, signups).execute();
+		
+		// If wanted, send cancellation code via email
+		if (!Strings.isNullOrEmpty(email)) {
+			try {
+				sendCancellationCodeViaEmail(email, dogpark.get(), newSignup);
+			} catch (Exception ex) {
+				logger.error(ex.toString());
+			}
+		}
 
+		flashScope.put("signupSuccessfulMsg", getCancellationCodeMessage(dogpark.get(), newSignup));
 		return Results.redirect("/dogparks/" + dogparkId);
 	}
 
-	public Result cancelSignup() {
-		return Results.html();
+	public Result doCancelSignupPost(
+			FlashScope flashScope,
+			@Param("cancellationCode") String publicCancellationCode) throws RiakRetryFailedException {
+		
+		boolean cancelSuccessful = false;
+		
+		String decoded;
+		try {
+			decoded = new String(
+					Base64.getDecoder().decode(publicCancellationCode),
+					StandardCharsets.ISO_8859_1
+			);
+		} catch (IllegalArgumentException iaex) {
+			decoded = "";
+		}
+		
+		String[] parts = decoded.split(":");
+		if (parts.length >= 2) {
+			String dogparkId = parts[0];
+			String cancellationCode = parts[1];
+
+			DogparkSignupList signups = bucket.fetch(dogparkId, DogparkSignupList.class).execute();
+			if (signups != null) {
+				Optional<DogparkSignup> cancellableSignup = signups.stream()
+						.filter(signup -> signup.cancellationCode.equals(cancellationCode))
+						.findFirst();
+				if (cancellableSignup.isPresent()) {
+					signups.remove(cancellableSignup.get());
+					bucket.store(dogparkId, signups).execute();
+					cancelSuccessful = true;
+					
+					Optional<Dogpark> dogpark = getDogparkFromDb(dogparkId);
+					flashScope.put("cancellationMsg", getCancellationSuccessMsg(dogpark.get(), cancellableSignup.get()));
+				}
+			}
+		}
+
+		if (cancelSuccessful) {
+			flashScope.success("cancel.success");
+		} else {
+			flashScope.error("cancel.invalidCancellationCode");
+		}
+		return Results.redirect("/cancel");
 	}
 
-	private Optional<Dogpark> getDogpark(String dogparkId) throws RiakRetryFailedException {
+	private Optional<Dogpark> getDogparkFromDb(String dogparkId) throws RiakRetryFailedException {
 		Optional<Dogpark> dogpark = bucket.fetch(KEY_CITIES, CityList.class).execute().stream()
 				.flatMap(city -> city.dogparks.stream())
 				.filter(park -> dogparkId.equalsIgnoreCase(park.id))
 				.findFirst();
 		return dogpark;
 	}
-
+	
+	private void sendCancellationCodeViaEmail(String email, Dogpark dogpark, DogparkSignup signup) throws Exception {
+		Mail mail = mailProvider.get();
+		mail.setSubject("Peruutuskoodi");
+		mail.addTo(email);
+		mail.setFrom("koirapuistomiitit@gmail.com");
+		mail.setCharset("utf-8");		
+		mail.setBodyText(getCancellationCodeMessage(dogpark, signup));
+		postoffice.send(mail);
+	}
+	
+	private static String formPublicCancellationCode(String dogparkId, DogparkSignup signup) {
+		String rawCode = dogparkId + ":" + signup.cancellationCode;
+		String result = Base64.getEncoder().encodeToString(rawCode.getBytes(StandardCharsets.ISO_8859_1));
+		return result;
+	}
+	
+	private static String getCancellationCodeMessage(Dogpark dogpark, DogparkSignup signup) {
+		String publicCancellationCode = formPublicCancellationCode(dogpark.id, signup);
+		return "Peruutuskoodisi koirapuiston \"" + dogpark.name + "\" ilmoittautumiseen \"" 
+				+ signup.toString() + "\" on: " + publicCancellationCode;
+	}
+	
+	private static String getCancellationSuccessMsg(Dogpark dogpark, DogparkSignup signup) {
+		String publicCancellationCode = formPublicCancellationCode(dogpark.id, signup);
+		return "Peruutettu koirapuiston \"" + dogpark.name + "\" ilmoittautuminen \"" 
+				+ signup.toString() + "\".";
+	}
+	
 }
